b89e0554f4e4fd86d34a727e3a8068314ad8531d
Daniel Mulholland <dan.mulholland@gmail.com>
2026-04-04 00:39:47 +1000

    Documentation experiment

--- src/oscd-editor-cleanup.ts
+++ src/oscd-editor-cleanup.ts
@@ -1,5 +1,5 @@
 import { css, html, LitElement, TemplateResult } from 'lit';
-import { property, query } from 'lit/decorators.js';
+import { property, query, state } from 'lit/decorators.js';
 
 import { ScopedElementsMixin } from '@open-wc/scoped-elements/lit-element.js';
 
@@ -7,6 +7,11 @@ import OscdSclDialogs from '@omicronenergy/oscd-scl-dialogs/OscdSclDialogs.js';
 import type { EditWizard } from '@omicronenergy/oscd-scl-dialogs/OscdSclDialogs.js';
 import { OscdEditDialogEvents } from '@omicronenergy/oscd-scl-dialogs/oscd-scl-dialogs-events.js';
 import { newEditEventV2 } from '@openscd/oscd-api/utils.js';
+
+import { OscdDialog } from '@omicronenergy/oscd-ui/dialog/OscdDialog.js';
+import { OscdIconButton } from '@omicronenergy/oscd-ui/iconbutton/OscdIconButton.js';
+import { OscdIcon } from '@omicronenergy/oscd-ui/icon/OscdIcon.js';
+import { OscdFilledButton } from '@omicronenergy/oscd-ui/button/OscdFilledButton.js';
 
 import { CleanupDatasets } from './cleanup/datasets-container.js';
 import { CleanupControlBlocks } from './cleanup/control-blocks-container.js';
@@ -19,6 +24,10 @@ export default class OscdEditorCleanup extends ScopedElementsMixin(LitElement) {
     'cleanup-control-blocks': CleanupControlBlocks,
     'cleanup-data-types': CleanupDataTypes,
     'oscd-scl-dialogs': OscdSclDialogs,
+    'oscd-dialog': OscdDialog,
+    'oscd-icon-button': OscdIconButton,
+    'oscd-icon': OscdIcon,
+    'oscd-filled-button': OscdFilledButton,
   };
 
   /** The document being edited as provided to plugins by [[`OpenSCD`]]. */
@@ -30,6 +39,9 @@ export default class OscdEditorCleanup extends ScopedElementsMixin(LitElement) {
 
   @property({ attribute: false })
   docVersion?: unknown;
+
+  @state()
+  private helpOpen = false;
 
   @query('oscd-scl-dialogs')
   private sclDialogs!: OscdSclDialogs;
@@ -59,6 +71,9 @@ export default class OscdEditorCleanup extends ScopedElementsMixin(LitElement) {
     super.disconnectedCallback();
   }
 
+  private static readonly DOCS_URL =
+    'docs/oscd-editor-cleanup/dev/introduction.html';
+
   render(): TemplateResult {
     return html`
       <div class="cleanup">
@@ -75,7 +90,44 @@ export default class OscdEditorCleanup extends ScopedElementsMixin(LitElement) {
           .doc=${this.doc}
         ></cleanup-data-types>
       </div>
+
       <oscd-scl-dialogs></oscd-scl-dialogs>
+
+      <oscd-icon-button
+        class="help-button"
+        label="Help"
+        @click=${() => {
+          this.helpOpen = true;
+        }}
+      >
+        <oscd-icon>help_outline</oscd-icon>
+      </oscd-icon-button>
+
+      <oscd-dialog
+        class="help-dialog"
+        ?open=${this.helpOpen}
+        @close=${() => {
+          this.helpOpen = false;
+        }}
+      >
+        <div slot="headline">Help</div>
+        <div slot="content" class="help-content">
+          <iframe
+            src=${OscdEditorCleanup.DOCS_URL}
+            title="OpenSCD Cleanup Plugin Documentation"
+            sandbox="allow-scripts allow-same-origin allow-forms allow-popups"
+          ></iframe>
+        </div>
+        <div slot="actions">
+          <oscd-filled-button
+            @click=${() => {
+              this.helpOpen = false;
+            }}
+          >
+            Close
+          </oscd-filled-button>
+        </div>
+      </oscd-dialog>
     `;
   }
 
@@ -117,6 +169,36 @@ export default class OscdEditorCleanup extends ScopedElementsMixin(LitElement) {
       overflow: hidden;
     }
 
+    .help-button {
+      position: fixed;
+      top: 8px;
+      right: 8px;
+      z-index: 100;
+      --md-icon-button-icon-size: 20px;
+      --md-icon-button-container-size: 32px;
+      color: var(--oscd-base0, rgba(0, 0, 0, 0.54));
+    }
+
+    .help-dialog {
+      width: 100%;
+      height: 100%;
+      max-width: 90vw !important;
+      max-height: 90vh !important;
+    }
+
+    .help-content {
+      width: 100%;
+      height: calc(90vh - 80px);
+      box-sizing: border-box;
+    }
+
+    .help-content iframe {
+      width: 100%;
+      height: 100%;
+      border: none;
+      background: white;
+    }
+
     @media (max-width: 799px) {
       cleanup-datasets,
       cleanup-control-blocks,
@@ -124,6 +206,15 @@ export default class OscdEditorCleanup extends ScopedElementsMixin(LitElement) {
         flex: 1 1 100%;
         height: auto;
         min-height: 480px;
+      }
+
+      .help-dialog {
+        max-width: 100vw !important;
+        max-height: 90vh !important;
+      }
+
+      .help-content {
+        height: calc(90vh - 80px);
       }
     }
   `;
